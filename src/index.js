83a53edb8bfcf287a83557c5dee43776f15731db
DZ521111 <dhruvkakadiya3@gmail.com>
2021-06-11 08:17:27 +0530

    nested component!

--- src/index.js
+++ src/index.js
@@ -1,17 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// importing React from react for build a component
+// This index.js file is a entry point of our javascript code!
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import React from "react";
+// ReactDom used for render the first component of our project
+import ReactDom from "react-dom";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// This component called stateless functional component or dumped component.
+// Always return JXS
+function Greeting() {
+	return (
+		<div>
+			<h1>Hello, The Dz! </h1>
+			<Person />
+			<Message />
+		</div>
+	);
+}
+
+// Arrow function with implicit return
+const Person = () => <h3>Person function!</h3>;
+
+// Arrow function with explicit return
+const Message = () => <h3>Message function!</h3>;
+
+// arguments are what is the component which you wants to render and where to render
+// we have <div id = "root"></div> in index.html which is main .html file in public folder
+ReactDom.render(<Greeting />, document.getElementById("root"));
+
+// This is one more method without JSX that we can fire html tage.
+// const Greeting = () => {
+// 	return (React.createElement('h1', {}, 'Hello World!'));
+// }
+
+// Now if we want to add parent and child tag by using this method then
+// const Greeting = () => {
+// 	return React.createElement("div", {}, React.createElement("h1", {}, "Hello World"));
+// };
