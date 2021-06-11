98a6b8ce654c3b78a52a72a440e44d167fff95e5
DZ521111 <dhruvkakadiya3@gmail.com>
2021-06-11 11:30:51 +0530

    props updated mini book!

--- src/index.js
+++ src/index.js
@@ -8,17 +8,25 @@ import ReactDom from "react-dom";
 import "./index.css";
 
 // we are going to pass this variable through props
-const img = "https://m.media-amazon.com/images/I/51NhBr756lL._AC_UY327_FMwebp_QL65_.jpg";
-const title = "Out There: A Scientific Guide to Alien Life, Antimatter, and Human Space Travel (For the Cosmically Curious)";
-const author = "by Michael Wall and Karl Tate | 13 November 2018";
+const firstBook = {
+	img: "https://m.media-amazon.com/images/I/51NhBr756lL._AC_UY327_FMwebp_QL65_.jpg",
+	title: "Out There: A Scientific Guide to Alien Life, Antimatter, and Human Space Travel (For the Cosmically Curious)",
+	author: "by Michael Wall and Karl Tate | 13 November 2018",
+};
+
+const secondBook = {
+	img: "https://m.media-amazon.com/images/I/81QDeZ1h0pL._AC_UY327_FMwebp_QL65_.jpg",
+	title: "Space - Constellations: Knowledge Encyclopedia For Children",
+	author: "by Wonder House Books  | 1 January 2020",
+};
 
 // This component called stateless functional component or dumped component.
 // Always return JXS
 function BookList() {
 	return (
 		<section className="booklist">
-			<Book img={img} title={title} author={author} />
-			<Book img={img} title={title} author={author} />
+			<Book img={firstBook.img} title={firstBook.title} author={firstBook.author} />
+			<Book img={secondBook.img} title={secondBook.title} author={secondBook.author} />
 		</section>
 	);
 }
@@ -28,7 +36,7 @@ const Book = (props) => {
 		<article className="book">
 			<img src={props.img} alt="" />
 			<h3>{props.title}</h3>
-			<h4>{props.author}</h4>
+			<h4>{props.author.toUpperCase()}</h4>
 		</article>
 	);
 };
