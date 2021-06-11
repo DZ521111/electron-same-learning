47903d227857aa4374285dd4cb1a56e7ed59d003
DZ521111 <dhruvkakadiya3@gmail.com>
2021-06-11 16:16:28 +0530

    events updated!

---
+++ src/components/AllBooks.js
@@ -0,0 +1,21 @@
+// we are going to pass this variable through props
+export const allBooks = [
+	{
+		id: 1,
+		img: "https://m.media-amazon.com/images/I/51NhBr756lL._AC_UY327_FMwebp_QL65_.jpg",
+		title: "Out There: A Scientific Guide to Alien Life, Antimatter, and Human Space Travel",
+		author: "by Michael Wall and Karl Tate | 13 November 2018",
+	},
+	{
+		id: 2,
+		img: "https://m.media-amazon.com/images/I/81QDeZ1h0pL._AC_UY327_FMwebp_QL65_.jpg",
+		title: "Space - Constellations: Knowledge Encyclopedia For Children",
+		author: "by Wonder House Books  | 1 January 2020",
+	},
+	{
+		id: 3,
+		img: "https://m.media-amazon.com/images/I/51sX5WhsXLL._AC_UY327_FMwebp_QL65_.jpg",
+		title: "Space - 500 Facts",
+		author: "by Pegasus  | 18 May 2018",
+	},
+];
--- src/index.js
+++ src/index.js
@@ -5,29 +5,9 @@ import React from "react";
 // ReactDom used for render the first component of our project
 import ReactDom from "react-dom";
 // Impoting css file index.css
-import "./index.css";
-
-// we are going to pass this variable through props
-const allBooks = [
-	{
-		id: 1,
-		img: "https://m.media-amazon.com/images/I/51NhBr756lL._AC_UY327_FMwebp_QL65_.jpg",
-		title: "Out There: A Scientific Guide to Alien Life, Antimatter, and Human Space Travel",
-		author: "by Michael Wall and Karl Tate | 13 November 2018",
-	},
-	{
-		id: 2,
-		img: "https://m.media-amazon.com/images/I/81QDeZ1h0pL._AC_UY327_FMwebp_QL65_.jpg",
-		title: "Space - Constellations: Knowledge Encyclopedia For Children",
-		author: "by Wonder House Books  | 1 January 2020",
-	},
-	{
-		id: 3,
-		img: "https://m.media-amazon.com/images/I/51sX5WhsXLL._AC_UY327_FMwebp_QL65_.jpg",
-		title: "Space - 500 Facts",
-		author: "by Pegasus  | 18 May 2018",
-	},
-];
+import "./css/index.css";
+import { allBooks } from "./components/AllBooks";
+import Book from "./components/Book";
 
 // This component called stateless functional component or dumped component.
 // Always return JXS
@@ -43,47 +23,6 @@ function BookList() {
 		</section>
 	);
 }
-
-const Book = ({ img, title, author }) => {
-	// console.log(props);
-	// const { img, title, author } = props;
-	// eventHander => onClick and onMouseOver
-	const clickHanlder = () => {
-		alert("Hello Dz!");
-	};
-	// const complexClick = () => {
-	// 	// alert(author);
-	// 	console.log(author);
-	// };
-	const complexClick = (author) => {
-		// alert(author);
-		console.log(author);
-	};
-
-	return (
-		<article className="book">
-			<img src={img} alt="" />
-			<h3>{title}</h3>
-			<h4>{author}</h4>
-			<button type="button" onClick={clickHanlder}>
-				reference
-			</button>
-			{/* <button type="button" onClick={complexClick}>
-				author
-			</button> */}
-			{/* This method used when you must have to passing a argument to the events. */}
-			<button
-				type="button"
-				onClick={() => {
-					complexClick(author);
-				}}
-			>
-				author
-			</button>
-			{/* {props.children} */}
-		</article>
-	);
-};
 
 // const Image = () => {
 // 	return ;
