59a4bace78aa71427c28e477e50200156ee6798e
DZ521111 <dhruvkakadiya3@gmail.com>
2021-06-11 08:43:15 +0530

    updated mini book!

--- src/index.js
+++ src/index.js
@@ -16,9 +16,26 @@ function BookList() {
 }
 
 const Book = () => {
-	return <article>This is Book!</article>;
+	return (
+		<article>
+			<Image />
+			<Title />
+			<Author />
+		</article>
+	);
 };
 
+const Image = () => {
+	return <img src="https://m.media-amazon.com/images/I/51NhBr756lL._AC_UY327_FMwebp_QL65_.jpg" alt="" />;
+};
+
+const Title = () => {
+	return <h3>Out There: A Scientific Guide to Alien Life, Antimatter, and Human Space Travel (For the Cosmically Curious)</h3>;
+};
+
+const Author = () => {
+	return <h3>by Michael Wall and Karl Tate | 13 November 2018</h3>;
+};
 // // Arrow function with implicit return
 // const Person = () => <h3>Person function!</h3>;
 
